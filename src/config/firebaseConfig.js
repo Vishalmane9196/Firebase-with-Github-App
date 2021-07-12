9f7b1ec5a40657c3e8bc0f0d847cd6a1dee598d7
vishalmane9196 <vishalmane9196@gmail.com>
2021-07-12 11:19:20 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDcy7qxw2eIXRGM8ShNFlKXCTxSUlIPKa4",
+    authDomain: "gitauth-e87ec.firebaseapp.com",
+    projectId: "gitauth-e87ec",
+    storageBucket: "gitauth-e87ec.appspot.com",
+    messagingSenderId: "592975672149",
+    appId: "1:592975672149:web:a608a0a41eb1a6defcb5dc",
+    measurementId: "G-E39PSN89E8"
+};
